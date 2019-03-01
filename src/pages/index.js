3a17f1a9a568f93cc5f7fb797b75e293c49a28c0
Kate <kate@192-168-1-7.tpgi.com.au>
2019-03-02 09:53:08 +1100

    Add styled components and get index structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: ${props => props.direction && props.direction || 'row'};
+`;
+
+const BigSection = ({ title, buttonText }) => {
+  return (
+    <Flex direction="column">
+      <h2>{title}</h2>
+      <div>Image place holder</div>
+      <button>{buttonText}</button>
+    </Flex>
+  );
+};
+
+const Wrapping = () => <BigSection title="Wrapping" buttonText="More about wrapping" />
+const DashCam = () => <BigSection title="DashCam" buttonText="More about DashCam" />
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
     <h1>Home page</h1>
+    <Flex>
+      <Wrapping />
+      <DashCam />
+    </Flex>
   </div>
 )
